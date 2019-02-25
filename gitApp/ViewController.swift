7f0214e50c380e4fdd28a8749d0ca7f3f1a0be1f
Yarik Mihalko <yarik.mihalko@gmail.com>
2019-02-25 21:40:32 +0200

    Initial Commit

---
+++ gitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitApp
+//
+//  Created by Yarik Mihalko on 2/25/19.
+//  Copyright © 2019 Yarik Mykhalko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
